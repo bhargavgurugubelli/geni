83a5788b2012a47526388994b677d6edab38d9aa
bhargavgurugubelli <gurugubellibhargav@gmail.com>
2026-03-05 17:08:10 +0530

    debug homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,3 @@
-// app/page.tsx
-import Header from "@/components/Header/Header";
-import Hero from "@/components/Hero/Hero";
-import Differentiators from "@/components/Differentiators";
-import AtAGlance from "@/components/AtAGlance";
-import ParentQuotes from "@/components/ParentQuotes";
-import Footer from "@/components/Footer";
-
 export default function Home() {
-  return (
-    <>
-      <Header />
-      <Hero />
-      <Differentiators />
-      <AtAGlance />
-      <ParentQuotes />
-      <Footer />
-    </>
-  );
+  return <h1>Genesis Website Working</h1>;
 }
